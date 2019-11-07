6e60f5c53a38509c8a5d342a8311c41c84ab32fa
Duy Tu <duytuhubt@gmail.com>
2019-11-07 12:54:34 +0700

    add Layout,TaskController,TaskRequest

---
+++ resources/lang/en/setting.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'delete_success' => 'Delete Task Success!!!',
+    'add_success' => 'ADD Task Success!!!',
+    'name_required' => 'Input Task Name Null!!!',
+    'name_min' => 'Input Task Name Min! Pls Input >3 Char',
+    'name_max' => 'Input Task Name Max! Pls Input <10 Char',
+];
